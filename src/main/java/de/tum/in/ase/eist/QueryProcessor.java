dbef18defbe4058da86bbc4b529e4033a4b19773
ge45luw <ge45luw@mytum.de>
2022-07-15 15:24:18 +0200

    numbers

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,10 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Zuza";
-        } else { // TODO extend the programm here
+        } else if (query.contains("numbers")) {
+            return "20131";
+        } else {
+            // TODO extend the programm here
             return "";
         }
     }
